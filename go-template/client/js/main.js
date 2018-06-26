af719e9b866f986e985fea8191513862d7a63310
Verrol Adams <verrol@lorrev.org>
2018-06-25 22:48:59 -0400

    v010 - Go Template

---
+++ go-template/client/js/main.js
@@ -0,0 +1,6 @@
+// https://developer.mozilla.org/en-US/docs/Web/API/Server-sent_events/Using_server-sent_events
+// https://www.w3schools.com/html/html5_serversentevents.asp
+function onLoaded() {
+
+
+}
