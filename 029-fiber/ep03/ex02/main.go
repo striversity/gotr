47673adb483f96901b1f75b2491d1ddb03fcfa3d
Verrol Adams <verrol@lorrev.org>
2023-08-15 16:57:06 -0400

    ep29.03 - Go Fiber Web Framework - Part 3 - Fiber Immutable config field

---
+++ 029-fiber/ep03/ex02/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"log/slog"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+const grId = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+var idx = 0
+
+func getHandlerId() string {
+	c := grId[idx%26]
+	idx++
+	return fmt.Sprintf("grID-%v-%c", idx, c) // eg: grID-1-A, grID-2-B, grID-3-C, ...
+}
+
+func getHandler(c *fiber.Ctx) error {
+	ccId := getHandlerId()
+	n := c.Params("name")
+
+	doStuff(ccId, n)
+
+	slog.Info("request received", "name", n)
+	return nil
+}
+
+func doStuff(ccId string, n string) {
+	go func() {
+		slog.Info("starting handler", "ccId", ccId, "name", n)
+		t := time.After(10 * time.Second)
+		for {
+			select {
+			case <-t:
+				slog.Info("handler done", "ccId", ccId, "name", n)
+				return
+			default:
+				slog.Info("still running", "ccId", ccId, "name", n)
+				time.Sleep(1 * time.Second)
+			}
+		}
+	}()
+}
+
+func main() {
+	appConfig := fiber.Config{
+		AppName:           "My Awesome App v0.0.0-beta1",
+		EnablePrintRoutes: true,
+		ServerHeader:      "Awesome App 1",
+	}
+
+	app := fiber.New(appConfig)
+	app.Get("/:name", getHandler).Name("Get default")
+
+	err := app.Listen(":3000")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
